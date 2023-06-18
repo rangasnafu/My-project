b4b3f37729228d9cc574758ad43805d88b4b8c08
Lily M - Lab 2 102 <135566059+roguemammalstudent@users.noreply.github.com>
2023-06-19 08:52:58 +1000

    added timer countdown

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
+using TMPro;
+
+
+public class Timer : MonoBehaviour
+{
+    public float TimeLeft;
+    public bool TimerOn = false;
+
+    public TextMeshProUGUI TimerTxt;
+
+    void Start()
+    {
+        TimerOn = true;
+    }
+
+    void Update()
+    {
+        if (TimerOn)
+        {
+            if (TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+                updateTimer(TimeLeft);
+            }
+            else
+            {
+                //    Debug.Log("Time is UP!");
+                TimeLeft = 0;
+                TimerOn = false;
+            }
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        TimerTxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+}
