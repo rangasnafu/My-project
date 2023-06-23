153535d063b88f3d3aefc3fab196bc39760088a9
SneakyNinja2041 <135566030+SneakyNinja2041@users.noreply.github.com>
2023-06-23 13:11:17 +1000

    Timer colour and GameOverUI
    Timer changes to red when there is 3 seconds left, and there are different UI's for if you are caught by a guard, or if you run out of time so you know which made you lose.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -10,7 +10,7 @@ public class Timer : MonoBehaviour
     float currentTime = 0f;
     float startingTime = 10.4f;
 
-    public GameObject gameOverUI;
+    public GameObject gameOverTimeUI;
 
     public string newGameScene;
 
@@ -28,15 +28,20 @@ public class Timer : MonoBehaviour
         currentTime -= Time.deltaTime;
         countdownText.text = currentTime.ToString("0");
 
+        if (currentTime <=3.5)
+        {
+            countdownText.color = Color.red;
+        }
+
         if (currentTime < 0)
         {
             Debug.Log("GameOver!");
             Time.timeScale = 0f;
-            gameOverUI.SetActive(true);
+            gameOverTimeUI.SetActive(true);
 
         }
 
-        if (gameOverUI.activeSelf && Input.GetKeyDown(KeyCode.R))
+        if (gameOverTimeUI.activeSelf && Input.GetKeyDown(KeyCode.R))
         {
             Time.timeScale = 1f;
             Scene scene = SceneManager.GetActiveScene();
